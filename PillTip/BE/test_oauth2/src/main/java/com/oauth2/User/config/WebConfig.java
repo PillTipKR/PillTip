1c8f0e5285a7c300f9bcc7cc97cf79bd108d8eb9
mireutale <mireutale@gmail.com>
2025-06-03 15:32:50 +0900

    (#51) 소셜 로그인 기능 구현

--- PillTip/BE/test_oauth2/src/main/java/com/oauth2/User/config/WebConfig.java
+++ PillTip/BE/test_oauth2/src/main/java/com/oauth2/User/config/WebConfig.java
@@ -2,7 +2,9 @@
 // description : 앱, 웹 설정 / CORS
 package com.oauth2.User.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*") // 모든 요청 헤더 허용
                 .allowCredentials(true) // 인증 정보(쿠키, Authorization 헤더 등)를 클라이언트에서 전송 가능
                 .maxAge(3600); // CORS 사전 요청(pre-flight)의 캐시 시간을 초 단위로 설정 (1시간)
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 } 
