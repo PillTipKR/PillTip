995d3d22646aa714866899cd8b26da1ac507228b
mireutale <mireutale@gmail.com>
2025-06-03 15:32:46 +0900

    (#51) 폴더구조 재 수정 및 로그인 관련 로직 local test완료_버그 해결

--- PillTip/BE/test_oauth2/src/main/java/com/oauth2/Elasticsearch/Dto/ElasticsearchDTO.java
+++
@@ -1,6 +0,0 @@
-package com.oauth2.Elasticsearch.Dto;
-
-public record ElasticsearchDTO(
-        String type,   // "drug", "ingredient", "manufacturer"
-        String value   // 자동완성에 노출할 텍스트
-) {}
