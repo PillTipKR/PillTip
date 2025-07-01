32f39d4f2c2a9ae835292714533eb8ee5286df0f
gykim_kr <k_bright_st@pusan.ac.kr>
2025-07-01 23:24:35 +0900

    [FIX] 복약 UI 리팩토링 -author : 김기윤

--- PillTip/FE/app/src/main/java/com/pilltip/pilltip/composable/SearchComposable/DosageComposable.kt
+++ PillTip/FE/app/src/main/java/com/pilltip/pilltip/composable/SearchComposable/DosageComposable.kt
@@ -46,6 +46,7 @@ import com.pilltip.pilltip.composable.WheelColumn
 import com.pilltip.pilltip.composable.noRippleClickable
 import com.pilltip.pilltip.ui.theme.gray200
 import com.pilltip.pilltip.ui.theme.gray500
+import com.pilltip.pilltip.ui.theme.gray600
 import com.pilltip.pilltip.ui.theme.pretendard
 import com.pilltip.pilltip.ui.theme.primaryColor
 import com.pilltip.pilltip.ui.theme.primaryColor050
@@ -329,21 +330,18 @@ fun DayField(
             val isSelected = selectedDays[index]
             Box(
                 modifier = Modifier
-                    .size(40.dp)
-                    .clip(CircleShape)
-                    .border(
-                        width = 1.2.dp,
-                        color = if (isSelected) primaryColor else gray500,
-                        shape = CircleShape
-                    )
+                    .border(width = 1.dp, color = if(isSelected) primaryColor else Color.Transparent, shape = CircleShape)
+                    .padding(1.dp)
+                    .width(28.dp)
+                    .height(28.dp)
                     .noRippleClickable { onDayToggle(index) },
                 contentAlignment = Alignment.Center
             ) {
                 Text(
                     text = day,
                     fontFamily = pretendard,
-                    color = if (isSelected) primaryColor else gray500,
-                    fontWeight = FontWeight.Medium,
+                    color = if (isSelected) primaryColor else gray600,
+                    fontWeight = if(isSelected) FontWeight.W600 else FontWeight.W400,
                     fontSize = 14.sp
                 )
             }
