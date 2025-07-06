7db0441a7c4376c0507b64b478a680b5abddd4d0
kimgiyun <gykim_kr@naver.com>
2025-07-07 01:38:41 +0900

    [Feat] 알림 페이지 UI 1차 완료 - 통신 로직 테스트 완료 - author : 김기윤

--- PillTip/FE/app/src/main/java/com/pilltip/pilltip/view/main/CalenderPage.kt
+++ PillTip/FE/app/src/main/java/com/pilltip/pilltip/view/main/CalenderPage.kt
@@ -1,5 +1,10 @@
 package com.pilltip.pilltip.view.main
 
+import android.util.Log
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,16 +15,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -27,8 +38,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.pilltip.pilltip.calender.CalendarView
+import com.pilltip.pilltip.composable.DrugLogCard
+import com.pilltip.pilltip.composable.DrugLogDetailSection
 import com.pilltip.pilltip.composable.HeightSpacer
+import com.pilltip.pilltip.model.search.SearchHiltViewModel
 import com.pilltip.pilltip.ui.theme.gray050
 import com.pilltip.pilltip.ui.theme.gray800
 import com.pilltip.pilltip.ui.theme.pretendard
@@ -37,14 +52,24 @@ import java.time.LocalDate
 
 @Composable
 fun CalenderPage(
-
+    navController: NavController,
+    hiltViewModel: SearchHiltViewModel
 ) {
     var selectedDate by remember { mutableStateOf<LocalDate?>(LocalDate.now()) }
+    val logData by hiltViewModel.dailyDosageLog.collectAsState()
+    val selectedDrugLog = hiltViewModel.selectedDrugLog
+
+    LaunchedEffect(selectedDate) {
+        selectedDate?.let {
+            hiltViewModel.fetchDailyDosageLog(it)
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(gray050)
             .statusBarsPadding()
+            .verticalScroll(rememberScrollState())
     ) {
         Text(
             text = "복약일정",
@@ -61,7 +86,9 @@ fun CalenderPage(
         )
         CalendarView(
             selectedDate = selectedDate,
-            onDateSelected = { selectedDate = it }
+            onDateSelected = {
+                selectedDate = it
+            }
         )
         HeightSpacer(10.dp)
         Column(
@@ -80,7 +107,7 @@ fun CalenderPage(
         ) {
             HeightSpacer(36.dp)
             Text(
-                text = "${selectedDate?.month}월 ${selectedDate?.dayOfMonth}일",
+                text = "${selectedDate?.monthValue}월 ${selectedDate?.dayOfMonth}일",
                 style = TextStyle(
                     fontSize = 14.sp,
                     lineHeight = 21.sp,
@@ -105,8 +132,15 @@ fun CalenderPage(
                         color = gray800,
                     )
                 )
+                val animatedPercent by animateIntAsState(
+                    targetValue = logData?.percent ?: 0,
+                    animationSpec = tween(
+                        durationMillis = 500,
+                        easing = FastOutSlowInEasing
+                    )
+                )
                 Text(
-                    text = "71%",
+                    text = "${animatedPercent}%",
                     style = TextStyle(
                         fontSize = 28.sp,
                         lineHeight = 42.sp,
@@ -117,26 +151,37 @@ fun CalenderPage(
                 )
             }
             HeightSpacer(24.dp)
+            val animatedProgress by animateFloatAsState(
+                targetValue = (logData?.percent ?: 0) / 100f,
+                animationSpec = tween(
+                    durationMillis = 700,
+                    easing = FastOutSlowInEasing
+                )
+            )
             LinearProgressIndicator(
-                progress = { 70 / 100f },
+                progress = { animatedProgress },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(6.dp),
+                    .height(6.dp)
+                    .clip(RoundedCornerShape(50)),
                 color = primaryColor,
-                trackColor = Color(0x29787880)
+                trackColor = Color(0x29787880),
             )
             HeightSpacer(36.dp)
         }
-        LazyColumn(
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 22.dp, vertical = 24.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+            verticalArrangement = Arrangement.spacedBy(6.dp)
         ) {
-
-//            items(medicationList) { medication ->
-//                MedicationCard(medication = medication)
-//            }
+            if (selectedDrugLog == null) {
+                logData?.perDrugLogs?.forEach { drug ->
+                    selectedDate?.let { DrugLogCard(drug, hiltViewModel, it) }
+                }
+            } else {
+                selectedDate?.let { DrugLogDetailSection(selectedDrugLog, hiltViewModel, it) }
+            }
         }
     }
 }
